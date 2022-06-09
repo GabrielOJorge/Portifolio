84a1bc716df3300dd54018212592d1aedf1fd3a0
GabrielOJorge <goliver331@protonmail.com>
2022-06-09 14:29:47 -0300

    create tic-tac-toe page

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,6 @@
 const imageImg = document.querySelectorAll(".img-img");
 const imgOverlay = document.querySelectorAll(".img-overlay");
-const arrowBtn = document.querySelectorAll(".arrow-btn")
+const arrowBtn = document.querySelectorAll(".arrow-btn");
 
 for (let i = 0; i < imgOverlay.length; i++) {
   imgOverlay[i].addEventListener("mouseenter", () => {
@@ -10,4 +10,4 @@ for (let i = 0; i < imgOverlay.length; i++) {
   imgOverlay[i].addEventListener("mouseleave", () => {
     imageImg[i].style.opacity = "1"
   });
-}
+};
